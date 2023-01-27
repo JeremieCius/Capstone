d73b79b039e85b8900d73ae0468f1761a569b74c
JeremieCius <jeremiecius@gmail.com>
2023-01-27 10:36:47 -0700

    idk man

--- Surasshu/Surasshu/Program.cs
+++ Surasshu/Surasshu/Program.cs
@@ -38,14 +38,15 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthentication();
+
 app.UseAuthorization();
+app.MapRazorPages();
+
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapRazorPages();
 
 app.Run();
