b0517a8a7421461828faf31b89c30beee1c4a72b
JeremieCius <jeremiecius@gmail.com>
2023-02-01 13:50:11 -0700

    Gotta do keys :(

--- Surasshu/Surasshu/Data/SurasshuContext.cs
+++ Surasshu/Surasshu/Data/SurasshuContext.cs
@@ -17,7 +17,11 @@ namespace Surasshu.Data
 
         public DbSet<SurasshuUser> AspNetUsers { get; set; }
 
-        public DbSet<IdentityUserClaim<string>> UserClaims { get; set; }
+        public DbSet<IdentityUserClaim<string>> AspNetUserClaims { get; set; }
+
+        public DbSet<IdentityUserToken<string>> UserTokens { get; set; }
+
+        public DbSet<IdentityUserRole<string>> Roles { get; set; } 
 
         public DbSet<Quirk> Quirks { get; set; }
 
